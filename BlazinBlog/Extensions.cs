79a269d2f9f4ad913430ea9f28d5e4b1b5c5983f
Kivanc ERTURK <erturkkivanc@gmail.com>
2024-02-08 02:19:46 +0300

    Login logic

---
+++ BlazinBlog/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace BlazinBlog
+{
+    public static class Extensions
+    {
+        public static string GetUserName(this ClaimsPrincipal principal) => principal.FindFirstValue(AppConstants.ClaimNames.FullName)!;
+
+        public static string GetUserId(this ClaimsPrincipal principal) => principal.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+    }
+}
