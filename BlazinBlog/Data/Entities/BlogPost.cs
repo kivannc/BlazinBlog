2c4a4714514c176bebfc5e67fff3933467b3fb30
Kivanc Erturk <erturkkivanc@gmail.com>
2024-02-22 00:39:29 +0300

    Fix code formatting and add new ManageBlogPosts page

--- BlazinBlog/Data/Entities/BlogPost.cs
+++ BlazinBlog/Data/Entities/BlogPost.cs
@@ -5,16 +5,18 @@ namespace BlazinBlog.Data.Entities;
 public class BlogPost
 {
     public int Id { get; set; }
-    [Required,MaxLength(100)]
+    [Required, MaxLength(100)]
     public string Title { get; set; }
     [MaxLength(125)]
     public string Slug { get; set; }
-    [Required,MaxLength(100)]
+    [Required, MaxLength(100)]
     public string Image { get; set; }
-    [Required,MaxLength(500)]
+    [Required, MaxLength(500)]
     public string Introduction { get; set; }
     [Required]
     public string Content { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Please select a valid category")]
     public int CategoryId { get; set; }
     public string UserId { get; set; }
 
